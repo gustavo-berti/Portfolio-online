52b1178d5662da487c9f511e56ffcb77fc3ad38c
gustavo-berti <gutigimenez15@gmail.com>
2025-03-24 21:18:25 -0300

    Finalizando seções e adicionando as paginas dos projetos

---
+++ script.js
@@ -0,0 +1,15 @@
+let btn_git = document.getElementById('git');
+let btn_fake_news = document.getElementById('fake-news');
+let btn_monitoring = document.getElementById('monitoring');
+
+btn_git.addEventListener('click',()=>{
+    location.href="project-git.html";
+})
+
+btn_fake_news.addEventListener('click',()=>{
+    location.href="project-fake-news.html";
+})
+
+btn_monitoring.addEventListener('click',()=>{
+    location.href="project-monitoring.html";
+})
