bee56c74aec119b3c844fecb3b4c1f871a9b784d
gustavo-berti <gutigimenez15@gmail.com>
2025-03-25 16:58:37 -0300

    Retirecionando para as redes sociais

--- script.js
+++ script.js
@@ -2,17 +2,33 @@ let btn_git = document.getElementById('git');
 let btn_fake_news = document.getElementById('fake-news');
 let btn_monitoring = document.getElementById('monitoring');
 
+let github = document.getElementById('github');
+let instagram = document.getElementById('instagram');
+let linkedin = document.getElementById('linkedin');
+
+github.addEventListener('click', ()=>{
+    location.href="https://github.com/gustavo-berti";
+});
+
+instagram.addEventListener('click', ()=>{
+    location.href="https://www.instagram.com/gustavo_gberti/";
+});
+
+linkedin.addEventListener('click', ()=>{
+    location.href="https://www.linkedin.com/in/gustavo-gimenez-berti-70797a250/";
+});
+
 btn_git.addEventListener('click',()=>{
     location.href="project-git.html";
-})
+});
 
 btn_fake_news.addEventListener('click',()=>{
     location.href="project-fake-news.html";
-})
+});
 
 btn_monitoring.addEventListener('click',()=>{
     location.href="project-monitoring.html";
-})
+});
 
 document.getElementById('navbar-toggle').addEventListener('click', ()=>{
     const menu = document.getElementById('navbar-menu');
