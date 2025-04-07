98b4d680625b07b6ba0f114d55427c6d0bb7a349
gustavo-berti <gutigimenez15@gmail.com>
2025-04-07 20:18:23 -0300

    Colocando os projetos como modal

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const theme = localStorage.getItem('theme');
+    document.body.setAttribute("data-theme", theme);
+    toggleMode();
+});
+
+let modal;
+
 let toggle_button = document.getElementById('toggle-button');
 let toggle_icon = document.getElementById('toggle-icon');
 
 let github = document.getElementById('github');
 let instagram = document.getElementById('instagram');
 let linkedin = document.getElementById('linkedin');
-
-function goToGit() {
-    location.href = "project-git.html";
-};
-
-function goToFakeNews() {
-    location.href = "project-fake-news.html";
-};
-
-function goToMonitoring() {
-    location.href = "project-monitoring.html";
-};
 
 try {
     instagram.addEventListener('click', () => {
@@ -32,7 +28,7 @@ try {
 
 try {
 
-    btn_git.addEventListener('click', () => {
+    github.addEventListener('click', () => {
         location.href = "https://github.com/gustavo-berti";
     });
 } catch (error) { }
@@ -61,4 +57,19 @@ function toggleMode() {
         body.setAttribute('data-theme', 'light');
         toggle_icon.src = "assets/moon.png"
     }
+    localStorage.setItem('theme', theme);
+}
+
+async function showModal(element) {
+    console.log(element);
+    modal = document.getElementById(element);
+    modal.classList.remove('hidden');
+    modal.addEventListener("click", (e) => {
+        if (e.target === modal)
+            closeModal();
+    });
+}
+
+function closeModal() {
+    modal.classList.add('hidden');
 }
